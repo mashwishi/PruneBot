648e4978c634a83fa1854a02446a61c5e5e412e4
Mashwishi <mathewagustinbella@gmail.com>
2021-07-30 16:33:45 +0800

    2.0.0

--- index.js
+++ index.js
@@ -1,50 +1,132 @@
-require ('dotenv').config()
-const Discord = require('discord.js');
-const { readdirSync } = require("fs");
-const handleEvents = require("./events");
+require("dotenv").config();
+const config = require("./config/config.json");
+const Enmap = require("enmap");
+const Discord = require("discord.js");
 
-const config = process.env;
-const bot = new Discord.Client({disableEveryone: true});
+const client = new Discord.Client({
+	partials: ["MESSAGE", "USER", "REACTION"],
+	disableMentions: "everyone"
+});
+const DisTube = require("distube");
 
-handleEvents(bot);
+client.config = config;
+global.client = client;
+client.login(process.env.TOKEN);
+global.nowyear = new Date().getFullYear();
+global.emojis = require("./config/emoji.json");
 
-bot.commands = new Discord.Collection();
-bot.aliases = new Discord.Collection();
+const db = require("quick.db");
+const { GiveawaysManager } = require("discord-giveaways");
 
-try
-{
-    let files = readdirSync("./commands/");
-    let jsfiles = files.filter(f => f.split(".").pop() === "js")
-    if(jsfiles.length <= 0) {
-        return console.log("[LOGS] Couldn't Find Commands!");
-    }
+const nz_date_string = new Date().toLocaleString("en-US", {
+	timeZone: "Asia/Hong_Kong"
+});
 
-    jsfiles.forEach(f => {
-        let pull = require(`./commands/${f}`);
-        bot.commands.set(pull.config.name, pull);
-        pull.config.aliases.forEach(alias => {
-            bot.aliases.set(alias, pull.config.name)
-        });
-    });
+client.commands = new Discord.Collection();
+client.slcommands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.emotes = emojis;
+client.colors = client.config.colors;
+client.snipes = new Map();
+client.mapss = new Map();
+client.mapss.set("uptimedate", nz_date_string);
 
-    bot.on("message", message => {
-        if(message.author.bot || message.channel.type === "dm") return;
+["command", "event", "music"].forEach(x =>
+	require(`./handlers/${x}.js`)(client)
+);
+["alwaysOn", "http"].forEach(x => require(`./server/${x}`)());
 
-        let { content } = message;
-        let { prefix } = config;
-        if(!content.startsWith(prefix)) return;
+client.settings = new Enmap({
+	name: "settings",
+	fetchAll: false,
+	autoFetch: true,
+	cloneLevel: "deep"
+});
 
-        let [ cmd, args ] = content.split(/\s(.+)/);
-        cmd = cmd.slice(prefix.length);
+client.moderationdb = new Enmap("moderation");
 
-        let commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd))
-        if(commandfile) commandfile.run(bot, message, args)
-    })
+client.distube = new DisTube(client, {
+	leaveOnFinish: true,
+	leaveOnEmpty: true,
+	leaveOnStop: true,
+	youtubeDL: true,
+	updateYouTubeDL: true,
+	youtubeCookie:
+		"GPS=1; YSC=w5dGoHzqQRI; VISITOR_INFO1_LIVE=B4ElBqxSDv4; PREF=tz=Asia.Hong_Kong"
+});
 
-    bot.login(process.env.TOKEN);
-}
-catch(error)
-{
-    console.log("Cannot load command files");
-    console.log(error);
-}
+if (!db.get("giveaways")) db.set("giveaways", []);
+
+const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
+	async getAllGiveaways() {
+		return db.get("giveaways");
+	}
+
+	async saveGiveaway(messageID, giveawayData) {
+		db.push("giveaways", giveawayData);
+		return true;
+	}
+
+	async editGiveaway(messageID, giveawayData) {
+		const giveaways = db.get("giveaways");
+		const newGiveawaysArray = giveaways.filter(
+			giveaway => giveaway.messageID !== messageID
+		);
+		newGiveawaysArray.push(giveawayData);
+		db.set("giveaways", newGiveawaysArray);
+		return true;
+	}
+
+	async deleteGiveaway(messageID) {
+		const newGiveawaysArray = db
+			.get("giveaways")
+			.filter(giveaway => giveaway.messageID !== messageID);
+		db.set("giveaways", newGiveawaysArray);
+		return true;
+	}
+};
+
+client.giveawaysManager = new GiveawayManagerWithOwnDatabase(client, {
+	storage: false,
+	updateCountdownEvery: 10000,
+	endedGiveawaysLifetime: 30000,
+	hasGuildMembersIntent: false,
+	default: {
+		botsCanWin: false,
+		exemptPermissions: ["MANAGE_MESSAGES", "ADMINISTRATOR"],
+		embedColor: "#ff6969",
+		embedColorEnd: "#505050",
+		reaction: "🎉"
+	}
+});
+
+client.status = queue =>
+	`Volume: \`${queue.volume}%\` | Filter: \`${
+		queue.filter || "Off"
+	}\` | Loop: \`${
+		queue.repeatMode
+			? queue.repeatMode == 2
+				? "All Queue"
+				: "This Song"
+			: "Off"
+	}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
+
+client.ws.on("INTERACTION_CREATE", async interaction => {
+	if (!client.slcommands.has(interaction.data.name)) return;
+	try {
+		client.slcommands.get(interaction.data.name).execute(interaction);
+	} catch (error) {
+		console.log(
+			`Error from command ${interaction.data.name} : ${error.message}`
+		);
+		console.log(`${error.stack}\n`);
+		client.api.interactions(interaction.id, interaction.token).callback.post({
+			data: {
+				type: 4,
+				data: {
+					content: "Sorry, error occurred when running this command!"
+				}
+			}
+		});
+	}
+});
