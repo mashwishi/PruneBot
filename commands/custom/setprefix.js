c2423e33287c2c4fe02d957e7d873b207794a835
Mashwishi <mathewagustinbella@gmail.com>
2021-08-01 13:52:37 +0800

    2.0.0
    Value to Prefix

--- commands/custom/setprefix.js
+++ commands/custom/setprefix.js
@@ -81,7 +81,7 @@ module.exports.run = async (client, message, args) => {
 module.exports.help = {
 	name: "setprefix",
 	description: "This command is used for changing the prefix.",
-	usage: "p!setprefix <value>",
+	usage: "p!setprefix <prefix>",
 	accessableby: "Manage Server",
 	aliases: []
 };
